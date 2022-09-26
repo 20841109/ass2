8b06188a1a7cf60eba065f165e1fe4a53bf1c153
20841109 <indhureddy12@hotmail.com>
2022-09-26 21:59:46 +1000

    recent2

--- src/app.js
+++ src/app.js
@@ -25,11 +25,117 @@ app.get('/', (req, res) => {
       <head>
         <link rel="stylesheet" type="text/css" href="/assets/css/app.css">
       </head>
+      <style>
+		.idle
+			{
+				background-color:#e7e7e7
+			}
+		.selected
+			{
+				background-color:#008CBA
+			}
+      
+
+      </style>
+
       <body>
         <div id="root"></div>
         <script src="/assets/js/vendor.js"></script>
         <script src="/assets/js/app.js"></script>
-      </body>
+        
+        Clicked cell is :<a class ="selected" id="clicked"> </a><br>
+        move:<a id="counter"></a>
+        </body>
+
+      
+
+
+    
+
+
+<table border=1>
+<colgroup>
+<col style="width: 30px">
+<col style="width: 30px">
+<col style="width: 30px">
+<col style="width: 30px">
+<col style="width: 30px">
+<col style="width: 30px">
+</colgroup>
+<thead>
+  <tr>
+    <td></td>
+    <td>A</td>
+    <td>B</td>
+    <td>C</td>
+    
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    
+    <td >1</td>
+    <td ><input type="button" id="A1" value="  " ></td>
+    <td ><input type="button" id="B1" value="  " ></td>
+    <td ><input type="button" id="C1" value="  " ></td>
+    
+  </tr>
+  <tr>
+    <td >2</td>
+    <td ><input type="button"   id ="A2" value="  "></td>
+    <td ><input type="button" id="B2" value="  "></td>
+    <td ><input type="button" id="C2" value="  "></td>
+    
+  </tr>
+  <tr>
+    <td >3</td>
+    <td ><input type="button" id="A3" value="  " ></td>
+    <td ><input type="button" id="B3" value="  " ></td>
+    <td ><input type="button" id="C3" value="  "></td>
+    
+  </tr>
+  
+</tbody>
+</table>		
+<br>
+
+<script>
+ 
+    	
+   var countervalue=0;
+   var onClicked=(myfunction) => {
+    countervalue++;
+     
+      x=myfunction.target.id;
+      if(countervalue%2==0){
+      document.getElementById(x).style.backgroundColor = '#008CBA';
+      
+      document.getElementById('clicked').innerHTML=myfunction.target.id;
+      document.getElementById('counter').innerHTML=countervalue;
+      document.getElementById(x).value="0";
+
+      
+      }
+      else
+      {
+        document.getElementById(x).style.backgroundColor = 'red';
+      document.getElementById('clicked').innerHTML=myfunction.target.id;
+      document.getElementById('counter').innerHTML=countervalue;
+      document.getElementById(x).value="X";
+      
+      }
+
+}
+window.addEventListener('click', onClicked);
+		
+  </script>
+</body>
+
+
+
+
+
+
     </html>`;
 
   // Respond with the HTML
